f115bf049fccdb7784373ea8848b14043941d7c7
Fabio Caseri <fabio.caseri@gmail.com>
2019-01-02 15:19:23 +0100

    Add configurable environment variable for listen port

--- ansible-vault-online.py
+++ ansible-vault-online.py
@@ -9,7 +9,7 @@ app = service.MultiService()
 web_root = resources.Root('./web')
 web_root.putChild(b'crypt', resources.Crypt())
 web_factory = server.Site(web_root)
-web_service = internet.TCPServer(8081, web_factory, interface='0.0.0.0')
+web_service = internet.TCPServer(int(os.getenv('PORT', 8081)), web_factory, interface='0.0.0.0')
 web_service.setServiceParent(app)
 
 application = service.Application("ansible-vault")
